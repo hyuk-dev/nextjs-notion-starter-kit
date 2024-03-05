98353d733c34b99d60307f9858a9ed4f27399963
hyuk-dev <139575548+hyuk-dev@users.noreply.github.com>
2024-03-05 10:11:51 +0900

    Update Footer.tsx
    푸터 다크모드 버튼 제거

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -36,21 +36,10 @@ export const FooterImpl: React.FC = () => {
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright {currentYear} {config.author}</div>
+      <div className={styles.copyright}>Copyright© {currentYear} {config.author}</div>
 
       <div className={styles.settings}>
-        {hasMounted && (
-          <a
-            className={styles.toggleDarkMode}
-            href='#'
-            role='button'
-            onClick={onToggleDarkMode}
-            title='Toggle dark mode'
-          >
-            {isDarkMode ? <IoMoonSharp /> : <IoSunnyOutline />}
-          </a>
-        )}
-      </div>
+      
 
       <div className={styles.social}>
         {config.twitter && (
